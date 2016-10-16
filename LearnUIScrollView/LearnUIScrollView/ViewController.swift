ae2ffc11eb6658e77002c065d6705adef845fe54
ADMIN <dungbkhn2015@gmail.com>
2016-10-17 00:14:35 +0700

    Vertically ScrollView and TapGestureRecognizer,alertController

--- LearnUIScrollView/LearnUIScrollView/ViewController.swift
+++ LearnUIScrollView/LearnUIScrollView/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
 
+    var images = [UIImageView]()
+    var contentWidth: CGFloat = 0.0
+    var newX:CGFloat = 0.0
+    //let gestureRecognizer = UITapGestureRecognizer(target: self, action: Selector(("handleImageDidTap:")))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 0...11 {
+            let image = UIImage (named: "image\(i).jpg")
+            let imageView = UIImageView (image: image)
+            images.append(imageView)
+            
+            newX = view.frame.midX + view.frame.width * CGFloat(i);
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - view.frame.width/2, y: 0, width: view.frame.width, height: view.frame.height)
+            
+        }
+        contentWidth = newX + view.frame.width/2;
+
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.height)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func handleImageDidTap(gestureRecognizer: UITapGestureRecognizer){
+        let alertController = UIAlertController(title: nil, message: "You are tapped at\(gestureRecognizer.location(in: self.scrollView))", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.cancel, handler: {(alert: UIAlertAction!) in print("Foo")}))
+        self.present(alertController, animated:true, completion: nil)
+    }
 }
 
